4333da391ca78bde799e2229413c89528b0c6a95
ssmiler <spencer.kitchen@wsu.edu>
2017-02-10 20:44:06 -0800

    2.2 complete: Creating a custom BoardView

---
+++ FifteenPuzzle/BoardView.swift
@@ -0,0 +1,21 @@
+//
+//  BoardView.swift
+//  FifteenPuzzle
+//
+//  Created by Spencer Kitchen on 2/10/17.
+//  Copyright © 2017 wsu.vancouver. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
